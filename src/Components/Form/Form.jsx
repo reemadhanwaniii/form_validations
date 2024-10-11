265854f2c7e4cbe6bb34dec78c66e45db4de3507
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-10-11 23:05:44 +0530

    basic app

---
+++ src/Components/Form/Form.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './Form.css';
+import validatePassword from '../../Helper/passwordValidator';
+import validateEmail from '../../Helper/emailValidator';
+
+function Form() {
+
+const [formValues,setFormValues] = useState({
+    email : "",
+    password : ""
+});
+
+const handleFormSubmit = (event) => {
+        event.preventDefault();
+        handleValidateEmail();
+        handleValidatePassword();
+}
+
+const handleValidatePassword = () =>{
+    const password = formValues.password;
+    if(!validatePassword(password)){
+        console.log("password doesn't contain valid parameters")
+    }
+}
+
+const handleValidateEmail = () => {
+    const email = formValues.email;
+    if(!validateEmail(email)){
+        console.log("email doesn't contain valid parameters")
+        //document.getElementById('email-input').focus();
+    }
+}
+
+return(
+    <div>
+        New Form
+        <form onSubmit={handleFormSubmit}>
+            <div className="wrapper email-wrapper">
+                <input 
+                    id="email-input"
+                    type="text" 
+                    value={formValues.email}
+                    onChange={(event) => setFormValues({...formValues,email:event.target.value})}
+                />
+            </div> 
+            <div className="wrapper password-wrapper">
+                <input 
+                    id="password-input"
+                    type="password" 
+                    value={formValues.password}
+                    onChange={(event) => setFormValues({...formValues,password:event.target.value})}
+                />
+            </div>
+            <button>Submit</button>
+        </form>
+    </div>
+)
+
+}
+
+export default Form;
