77674b319ad4f6ab9ec3a48fdec0b79a5a404a9c
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-11-05 18:43:47 +0530

    use uncontrolled component (useRef) for validation

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,16 +1,19 @@
 import './Form.css';
 import Input from '../Input/Input';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { FormContext } from '../Providers/formContext';
 
 function Form() {
 
 const {formInput} = useContext(FormContext);
-
+const emailRef = useRef(null);
+const passwordRef = useRef(null);
 
 const handleFormSubmit = (event) => {
         event.preventDefault();
         console.log(formInput);
+        //we have access to formInput that means validation can occur here
+        emailRef.current.focus(); //assume form is not validated
 }
 
 
@@ -18,12 +21,22 @@ const handleFormSubmit = (event) => {
 return(
     <div>
         New Form
-        <form onSubmit={handleFormSubmit}>
+        <form onSubmit={handleFormSubmit} noValidate>
             <div className="wrapper email-wrapper">
-                <Input type="text" id="email-input" label="email"/>
+                <Input 
+                    type="email" 
+                    id="email-input" 
+                    label="email" 
+                    inputRef={emailRef} //for user-defined component we can't directly passed ref here
+                />
             </div> 
             <div className="wrapper password-wrapper">
-                <Input type="password" id="password-input" label="password"/> 
+                <Input 
+                    type="password" 
+                    id="password-input" 
+                    label="password"
+                    inputRef={passwordRef}
+                /> 
             </div>
             <button>Submit</button>
         </form>
--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import { FormContext } from "../Providers/formContext";
 
-function Input({type, id, label}) {
+function Input({type, id, label, inputRef}) {
 
   const {formInput,setFormInput} = useContext(FormContext)
   const [text,setText] = useState("");
@@ -10,6 +10,7 @@ function Input({type, id, label}) {
         <input
             type={type}
             id={id}
+            ref={inputRef}
             value={text}
             onChange={(e) => {
                 setText(e.target.value);
